8fb9eee2fd247c35e114c2d4d75df532abbc5642
RaibeartRuadh <38784332+RaibeartRuadh@users.noreply.github.com>
2020-12-10 22:24:38 +0300

    Add files via upload

--- config/server.sh
+++ config/server.sh
@@ -1,7 +1,7 @@
 mkdir -p ~root/.ssh
 cp ~vagrant/.ssh/auth* ~root/.ssh
 hostnamectl set-hostname ipaserver.raibeart.loc
-yum update -y
+#yum update -y
 systemctl start firewalld
 systemctl enable firewalld
 firewall-cmd --permanent --add-port={80/tcp,443/tcp,389/tcp,636/tcp,88/tcp,464/tcp,53/udp,88/udp,464/udp,53/udp,123/udp}
